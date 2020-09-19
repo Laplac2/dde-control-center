79b4ad2a7f4e530caceeb95f7ff00839fe4b3592
Zhang Qipeng <zhangqipeng@uniontech.com>
2020-09-19 19:04:06 +0800

    fix
    Change-Id: I72ffffd00d29421caf3e5b51031832da3d4724e6

--- src/frame/modules/power/powermodel.cpp
+++ src/frame/modules/power/powermodel.cpp
@@ -193,7 +193,7 @@ void PowerModel::setBatteryPercentage(double batteryPercentage)
 
     m_batteryPercentage = batteryPercentage;
 
-    Q_EMIT batteryPercentageChanged(batteryPercentage);
+    Q_EMIT batteryPercentageChanged(true);
 }
 
 bool PowerModel::getDoubleCompare(const double value1, const double value2)
